09eec37120841244d296e02b5c4d0a8d10762c5c
baran beyazıt <byztmain2121@gmail.com>
2022-01-25 16:21:35 +0300

    proje is finished

---
+++ index.js
@@ -0,0 +1,85 @@
+
+
+
+
+
+$(document).ready(function(){
+
+    $("#item_1").click(function(){
+        $("body").css({
+    "background-color":"blue"
+        });
+        $("#item_1").css({
+            "border-color":"blue"
+                });
+      });
+
+      $("#item_2").click(function(){
+        $("body").css({
+            "background-color":"green"
+                });
+                $("#item_2").css({
+                    "color":"green"
+                        });
+
+      });
+
+      $("#item_3").click(function(){
+        $("body").css({
+            "background-color":"orange"
+                });
+                $("#item_3").css({
+                    "color":"orange"
+                        });
+      });
+
+      $("#item_4").click(function(){
+        $("body").css({
+            "background-color":"red"
+                });
+                $("#item_4").css({
+                    "color":"red"
+                        });
+      });
+
+      $("#item_5").click(function(){
+        $("body").css({
+            "background-color":"purple"
+                });
+                $("#item_5").css({
+                    "color":"purple"
+                        });
+      });
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
